ce9b4280761182e3ffdcd24b02a34e46966f3e72
KAZUYA　FARUKAWA <56099724+KAZUPRO@users.noreply.github.com>
2019-11-12 11:28:48 +0900

    全属性に対してセッター追加 (#8)
    セッター追加

---
+++ telledge/Models/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace telledge.Models
+{
+    public class Room : Controller
+    {
+        public int id { set; get; }
+        public int teacherId { set; get;}
+        public String roomName { set; get; }
+        public String tag { get; set; }
+        public String description { get; set; }
+        public int worstTime { get; set; }
+        public int extensionTime { get; set; }
+        public int point { get; set; }
+        public DateTime beginTime { get; set; }
+        public DateTime endTime { get; set; }
+
+    }
+}
