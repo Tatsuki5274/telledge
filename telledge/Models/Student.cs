3e2cad96adf543d609b3b4c770c2f761bd65ff77
KAZUYA　FARUKAWA <56099724+KAZUPRO@users.noreply.github.com>
2019-11-12 10:55:13 +0900

    Student (#6)
    * Student
    
    * Student
    
    * Student
    
    * Student全属性追加・修正
    
    大文字修正
    
    * 問題を修正
    
    * 関連ファイルの修正

---
+++ telledge/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace telledge.Models
+{
+    public class Student : Controller
+    {
+        public int id {get;}
+        public String name { get; set;}
+        public String mailaddress { get; set; }
+        public String skypeId { get; set; }
+        public int passwordDigest { get; set; }
+        public bool is2FA { get; set; }
+        public int point { get; set; }
+        public DateTime inactiveDate {get; set;}
+    }
+}
