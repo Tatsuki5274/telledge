e8090fedf33961d0298fffe1c23ec40cda5c15d7
Tatsuki Watanabe <42799529+Tatsuki5274@users.noreply.github.com>
2019-11-14 11:05:28 +0900

    アクタでURLのコントローラをネストするように設定を変更 (#14)
    * roomsコントローラの雛形を作成
    
    * 途中
    
    * 参考サイトを追加
    
    * アクタでURLをネストする設定を追加

--- telledge/Global.asax.cs
+++ telledge/Global.asax.cs
@@ -9,13 +9,19 @@ namespace telledge
 {
     // メモ: IIS6 または IIS7 のクラシック モードの詳細については、
     // http://go.microsoft.com/?LinkId=9394801 を参照してください
+    // https://stackoverflow.com/questions/19777880/controller-with-same-name-as-an-area-asp-net-mvc4 を参考
 
     public class MvcApplication : System.Web.HttpApplication
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                "Student",
+                "student/{controller}/{action}/{id}",
+                new { id = UrlParameter.Optional },
+                new[] { "telledge.Controllers.Student" }
+            );
             routes.MapRoute(
                 "Default", // ルート名
                 "{controller}/{action}/{id}", // パラメーター付きの URL
