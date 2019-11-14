1f2a643ebbb8e72412efc94207a268dd2c86506d
KAZUYA　FARUKAWA <56099724+KAZUPRO@users.noreply.github.com>
2019-11-14 14:20:58 +0900

    Student ログインメソッド (#15)
    * Roomクラスの全属性追加
    
    * ログイン検証
    
    ログイン検証する
    
    * ログインメソッド
    
    * ログインメソッド
    
    * テストファイルを作成
    
    * データベース接続

--- telledge.Tests/Models/Student.cs
+++ telledge.Tests/Models/Student.cs
@@ -1,18 +1,45 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using telledge.Models;  //プロジェクトのテスト対象
+
 
 namespace telledge.Tests.Models
 {
     [TestClass]
-    public class Student
+    public class StudentTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LoginFailByBothStatus()
         {
+            //存在しないメールアドレスとパスワードでnullが返ってくるかどうかテスト
+            Student ret = Student.login("notexit", "example");
+            Assert.IsNull(ret);
+        }
+        [TestMethod]
+        public void LoginFailByMail()
+        {
+            //存在する名前と間違ったパスワードでnullが返ってくるかテスト
+            Student ret = Student.login("name", "example");
+            Assert.IsNull(ret);
+        }
+
+        [TestMethod]
+        public void LoginFailByPassword()
+        {
+            //存在しない名前と正しいパスワードでnullが返ってくるかテスト
+            Student ret = Student.login("wrongName", "password");
+            Assert.IsNull(ret);
+        }
+
+        [TestMethod]
+        public void LoginSuccess()
+        {
+            //var session = new Mock<HttpSessionStateBase>();
+            //controllerContext.Setup(p => p.HttpContext.Session).Returns(session.Object); 
+            Student ret = Student.login("name", "password");
+            Assert.IsNotNull(ret);
+            //Assert.AreEqual(ret,
+            
+           
         }
     }
 }
--- telledge/Models/Student.cs
+++ telledge/Models/Student.cs
@@ -2,6 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace telledge.Models
 {
@@ -25,5 +36,50 @@ namespace telledge.Models
         public int point { get; set; }
         //生徒退会日
         public DateTime inactiveDate {get; set;}
+
+        public static Student login(String mailaddress, String password) 
+        {
+            Student retStudent = null;
+            string cstr = ConfigurationManager.ConnectionStrings["Db"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(cstr))
+            {
+                if (retStudent.inactiveDate != null)
+                {
+                    return null;
+                }
+                string sql = "select * from Student where id = @id";
+                SqlDataAdapter adapter = new SqlDataAdapter(sql,connection);
+                adapter.SelectCommand.Parameters.Add("@id",SqlDbType.VarChar);
+                DataSet ds = new DataSet();
+                int cnt = adapter.Fill(ds,"Student");
+                if(cnt != 0){
+                    retStudent = new Student();
+                    DataTable dt = ds.Tables["Student"];
+                    retStudent.passwordDigest = (Byte[])dt.Rows[0]["passwordDigest"];
+                    //retStudent.inactiveDate = (DateTime)dt.Rows[0]["inactiveDate"]; 
+                }
+
+                byte[] input = Encoding.ASCII.GetBytes(password);
+                SHA256 sha = new SHA256CryptoServiceProvider();
+                byte[] hash_sha256 = sha.ComputeHash(input);
+                if (retStudent.passwordDigest == hash_sha256)
+                {
+                    HttpContext.Current.Session["Student"] = retStudent;
+                    return retStudent;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        /*public static  Student currentUser()
+        {
+            Student s = new Student();
+           (Student)Session["Student"];
+        }*/
     }
 }
+ //引数に渡されたメールアドレスを持つ生徒のパスワードダイジェストと引数の平文パスワードをSHA256でダイジェスト化したものを比較し、
+        //等しければ対応するStudentクラスのオブジェクトを返しセッション変数名"Student"のセッションにオブジェクトを登録する。
+        //等しくなければnullを返し、セッションへの登録は行わない。 また、退会日がnull以外の場合は無条件にnullを返す。
