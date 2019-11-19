e000bbf6d493bde9d512fff01faa991425d415b9
Tatsuki Watanabe <42799529+Tatsuki5274@users.noreply.github.com>
2019-11-19 10:05:24 +0900

    テストファイルの復元と参照の追加 (#23)

---
+++ UnitTest/Student/Student.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using telledge.Models;  //プロジェクトのテスト対象
+
+
+namespace telledge.Tests.Models
+{
+    [TestClass]
+    public class StudentTest
+    {
+        [TestMethod]
+        public void LoginFailByBothStatus()
+        {
+            //存在しないメールアドレスとパスワードでnullが返ってくるかどうかテスト
+            Student ret = Student.login("notexit", "example");
+            Assert.IsNull(ret);
+        }
+        [TestMethod]
+        public void LoginFailByMail()
+        {
+            //存在する名前と間違ったパスワードでnullが返ってくるかテスト
+            Student ret = Student.login("name", "example");
+            Assert.IsNull(ret);
+        }
+
+        [TestMethod]
+        public void LoginFailByPassword()
+        {
+            //存在しない名前と正しいパスワードでnullが返ってくるかテスト
+            Student ret = Student.login("wrongName", "password");
+            Assert.IsNull(ret);
+        }
+
+        [TestMethod]
+        public void LoginSuccess()
+        {
+            //var session = new Mock<HttpSessionStateBase>();
+            //controllerContext.Setup(p => p.HttpContext.Session).Returns(session.Object); 
+            Student ret = Student.login("name", "password");
+            Assert.IsNotNull(ret);
+            //Assert.AreEqual(ret,       
+        }
+
+        [TestMethod]
+        public void SetPassWordTest()
+        {
+            Student student = new Student();
+            student.setPassword("password");
+        }
+    }
+}
