37eae9e0ce1b67aaf9636c14f1bc335102d19f1d
KAZUYA　FARUKAWA <56099724+KAZUPRO@users.noreply.github.com>
2019-11-26 12:02:07 +0900

    TeacherRoomsCreateコントローラー作成 (#54)
    * RoomsCreateコントローラー作成
    
    * Callコントローラー作成
    
    * Roomsコントローラー作成
    
    * indexコントローラー作成

---
+++ telledge/Controllers/Teachers/RoomsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace telledge.Controllers.Teachers
+{
+    public class RoomsController : Controller
+    {
+        // GET: Rooms
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View("//Create");
+
+        }
+        [HttpGet]
+        public ActionResult Call(int roomid)
+        {
+
+        }
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("");
+        }
+    }
+}
