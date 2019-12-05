6bd395b56c8b21263152828f5bfd472d85bb1859
Tatsuki Watanabe <42799529+Tatsuki5274@users.noreply.github.com>
2019-12-05 17:31:38 +0900

    Timer.jsの作成 (#93)
    * Javascriptファイルの作成
    
    * TimerのJs修正
    
    * Timerの修正
    
    * Timerの完成、延長時間未定
    
    * 処理の分岐
    
    * 表示の修正
    
    * 色変更を追加
    
    * 不要なコードを削除
    
    * コメントを追加
    
    * element表示問題の解決
    
    * 最終版
    
    * 改行コードを変更
    
    * Timer.jsから起動処理を除外

---
+++ telledge/Scripts/Timer.js
@@ -0,0 +1,59 @@
+/*
+ * 構造例
+ * <タグ id="parent_selector引数">
+ *	<タグ id="timer-status"></タグ>
+ *	<タグ id="timer-count"></タグ>
+ * </タグ>
+ */
+
+class Timer{
+	constructor(parent_selector, mintime, overtime) {
+		this.selector = parent_selector;
+		this.mintime = mintime;
+		this.overtime = overtime;
+		this.status = 0;
+	}
+	setTimerAsEssential() {
+		this.sec = this.mintime * 60;
+		this.status = 1;
+	}
+	setTimerAsExtend() {
+		this.sec = this.overtime * 60;
+		$(this.selector).children('#timer-count').css('color', 'red');
+		$(this.selector).children('#timer-status').css('color', 'red');
+		$(this.selector).children('#timer-status').text("延長時間");
+		this.status = 2;
+	}
+	getStatusCode() {
+		//statusコードを返却する
+		return this.status;
+	}
+	getStatusDetail() {
+		//statusに応じた状態を返却する
+		let ret;
+		if (this.status == 0) ret = "Not started";
+		else if (this.status == 1) ret = "Essential";
+		else if (this.status == 2) ret = "Extend";
+		else ret = "All done";
+	}
+	showTime() {
+		//secを適切な形に変換して表示する
+		const min = this.sec / 60;
+		const sec = this.sec % 60;
+		$(this.selector).children('#timer-count').text(parseInt(min) + ":" + parseInt(sec));
+	}
+
+	setTimer() {
+		//secを減らす
+		let sec = this.sec;
+		const interval = setInterval(() => {
+			this.showTime();
+			this.sec--;
+			if (this.sec < 0) {
+				this.status++;
+				if (this.status == 1) this.setTimerAsEssential();
+				if (this.status == 2) this.setTimerAsExtend();
+			}
+		}, 1000);
+	}
+}
