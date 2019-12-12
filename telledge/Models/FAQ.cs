7dfca4aec80c561300a042791c6fc816f8105fd9
KAZUYA　FARUKAWA <56099724+KAZUPRO@users.noreply.github.com>
2019-12-12 13:43:52 +0900

    FAQクラスの作成 (#117)

---
+++ telledge/Models/FAQ.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace telledge.Models
+{
+	public class FAQ
+	{
+		public int id;//FAQID
+		public String question;//FAQ質問
+		public String answer;//FAQ回答
+	}
+}
