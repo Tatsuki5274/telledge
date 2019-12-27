c17e7e89fe4e8925e81297bead3f78eac881f7c2
KAZUYA　FARUKAWA <56099724+KAZUPRO@users.noreply.github.com>
2019-12-27 14:35:13 +0900

    生徒の登録情報変更画面の作成 (#177)
    * Viewファイルの作成
    
    * Viewの作成途中
    
    * editアクションの作成
    
    * formタグ追加
    
    * Updateコントローラーの作成途中
    
    * 変数名修正
    
    * 名前修正
    
    * 遷移先追加
    
    * コメントアウト追加
    
    * コメントアウト
    
    * 不要な代入を削除
    
    * 不要な代入を削除
    
    * 不要な代入の削除
    
    * 指摘点を修正
    
    Co-authored-by: dsa-xx <dsa>

--- telledge/Controllers/Students/HomesController.cs
+++ telledge/Controllers/Students/HomesController.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using telledge.Models;
 
 namespace telledge.Controllers.Students
 {
@@ -13,5 +14,39 @@ namespace telledge.Controllers.Students
         {
             return View();
         }
+		[HttpGet]
+		public ActionResult edit()
+		{
+			return View("/Views/Students/Homes/edit.cshtml");
+		}
+		public ActionResult update(String mailaddress,String name,String imagePath,String skypeId)
+		{
+			Student student = Student.currentUser();
+			if(mailaddress != "")//入力してあれば、引数をデータを保持する。
+			{
+				student.mailaddress = mailaddress;
+			}
+			if(name != "")//入力してあれば、引数をデータを保持する。
+			{
+				student.name = name;
+			}
+			if(imagePath != "")//入力してあれば、引数をデータを保持する。
+			{
+				student.profileImage = imagePath;
+			}
+			if(skypeId != "")//入力してあれば、引数をデータを保持する。
+			{
+				student.skypeId = skypeId;
+			}
+			bool check = student.Update();
+			if(check == true)
+			{
+				return RedirectToRoute("Student", new { controller = "Homes", Action = "mypage"});
+			}
+			else
+			{
+				return View("/Views/Students/Homes/edit.cshtml");
+			}
+		}
     }
 }
