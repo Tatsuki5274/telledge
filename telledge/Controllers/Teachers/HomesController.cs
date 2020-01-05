eb014781331742fb194c5ebdb1cae1252d83dc8c
Tatsuki Watanabe <42799529+Tatsuki5274@users.noreply.github.com>
2020-01-05 13:57:11 +0900

    講師のマイページの実装 (#187)
    * ファイルを作成
    
    * スタイルシートを追加
    
    * 読み込みを追加
    
    * アセットを追加
    
    * アセットをコンパイル
    
    * 外れている関連を追加
    
    * 謎のスタイル設定を削除
    
    * メニューページのグリッドシステム化
    
    * 不要な定義を削除
    
    * ディスプレイサイズの設定を強化
    
    * 基本形を追加
    
    * 邪魔なインラインの削除
    
    * デザイン調整
    
    * データモデルの追加
    
    * スタイルの修正
    
    * 実態の追加
    
    * ログイン必須の実装
    
    * スタイルの廃止
    
    * リンクの追加

--- telledge/Controllers/Teachers/HomesController.cs
+++ telledge/Controllers/Teachers/HomesController.cs
@@ -1,17 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using telledge.Models;
 
 namespace telledge.Controllers.Teachers
 {
     public class HomesController : Controller
     {
         // GET: Homes
-        public ActionResult Index()
+        public ActionResult mypage()
         {
-            return View();
-        }
+			if (Teacher.currentUser() == null) return RedirectToRoute("Teacher", new { controller = "Sessions", Action = "create" });
+            return View("/Views/Teachers/Homes/mypage.cshtml", Teacher.currentUser());
+
+		}
     }
 }
