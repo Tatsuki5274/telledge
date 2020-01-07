e97cd49db97348a5ebec8e1e1104d71e7e25f6ab
Tatsuki Watanabe <42799529+Tatsuki5274@users.noreply.github.com>
2020-01-07 09:35:57 +0900

    講師のプロフィール更新画面と処理を実装 (#189)
    * アクションの定義を追加
    
    * 編集ページのテンプレートを展開
    
    * 引数の名前を変更
    
    * 引数の追加
    
    * 不要な変更要素の削除
    
    * ファイルパスを修正
    
    * ラジオボタンを作成
    
    * テキストを日本語化
    
    * ヘルパーメソッドを解除
    
    * ファイル入力を追加
    
    * 必要な項目の追加
    
    * 更新処理の実装

--- telledge/Controllers/Teachers/HomesController.cs
+++ telledge/Controllers/Teachers/HomesController.cs
@@ -16,5 +16,38 @@ namespace telledge.Controllers.Teachers
             return View("/Views/Teachers/Homes/mypage.cshtml", Teacher.currentUser());
 
 		}
+
+		[HttpPost]
+		public ActionResult update(String imagePath ,String mailaddress ,String name, String nationality, int sex, String intoroduction)
+		{
+			Teacher teacher = Teacher.currentUser();
+			if (teacher != null)
+			{
+				if (mailaddress != "") teacher.mailaddress = mailaddress;
+				if (name != "") teacher.name = name;
+				if (nationality != "") teacher.nationality = nationality;
+				if (intoroduction != "") teacher.intoroduction = intoroduction;
+				teacher.sex = sex;
+
+				if (teacher.Update())
+				{
+					//正常系
+					return RedirectToRoute("teacher", new { controller = "homes", action = "mypage" });
+				}
+				else
+				{
+					//異常系
+					return RedirectToRoute("teacher", new { controller = "homes", action = "mypage" });
+				}
+			}
+			//ログインしていない場合にする処理
+			return RedirectToRoute("teacher", new { controller = "sessions", action = "create" });
+
+		}
+
+		public ActionResult edit()
+		{
+			return View("/Views/Teachers/Homes/edit.cshtml");
+		}
     }
 }
