add0d2203a791540c01c6551d23064d20a35c04f
k0101-gitkraken <56099721+k0101-gitkraken@users.noreply.github.com>
2020-01-16 09:29:13 +0900

    GetRoomsのメソッドの作成 (#205)

---
+++ UnitTest/Teacher/GetRooms.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using telledge.Models;  //プロジェクトのテスト対象
+
+namespace UnitTest.Teachers
+{
+    [TestClass]
+    public class TeacherGetRooms
+    {
+        [TestMethod]
+        public void TestGetRooms()
+        {
+			Teacher teacher = new Teacher();
+			teacher.id = 1;
+            Room[] test = teacher.getRooms();
+            Assert.IsNotNull(test);
+        }
+        [TestMethod]
+        public void TestGetRoomsFailed()
+        {
+			Teacher teacher = new Teacher();
+			teacher.id = 999;
+			Room[] test = teacher.getRooms();
+			Assert.IsNull(test);
+		}
+    }
+}
--- telledge/Models/Teacher.cs
+++ telledge/Models/Teacher.cs
@@ -10,136 +10,138 @@ using System.Web;
 namespace telledge.Models
 {
 	public class Teacher
-    {
-        //講師ID
-        public int id { set; get; }
-        //講師名
-        public String name { set; get; }
-        //講師性別
-        public int sex { set; get; }
-        //講師プロフィール画像
-        public String profileImage { set; get; }
-        //講師年齢
-        public int age { set; get; }
-        //講師の会話言語
-        public String language { set; get; }
-        //講師自己紹介
-        public String intoroduction { set; get; }
-        //講師のパスワードのダイジェスト
-        public byte[] passwordDigest { set; get; }
-        //講師メールアドレス
-        public String mailaddress { set; get; }
-        //講師保有ポイント
-        public int point { set; get; }
-        //講師住所
-        public String address { set; get; }
-        //二段階認証の有無
-        public Boolean is2FA { set; get; }
-        //講師の在住国籍
-        public String nationality { set; get; }
-        //講師退会日
-        public DateTime? inactiveDate { set; get; }
+	{
+		//講師ID
+		public int id { set; get; }
+		//講師名
+		public String name { set; get; }
+		//講師性別
+		public int sex { set; get; }
+		//講師プロフィール画像
+		public String profileImage { set; get; }
+		//講師年齢
+		public int age { set; get; }
+		//講師の会話言語
+		public String language { set; get; }
+		//講師自己紹介
+		public String intoroduction { set; get; }
+		//講師のパスワードのダイジェスト
+		public byte[] passwordDigest { set; get; }
+		//講師メールアドレス
+		public String mailaddress { set; get; }
+		//講師保有ポイント
+		public int point { set; get; }
+		//講師住所
+		public String address { set; get; }
+		//二段階認証の有無
+		public Boolean is2FA { set; get; }
+		//講師の在住国籍
+		public String nationality { set; get; }
+		//講師退会日
+		public DateTime? inactiveDate { set; get; }
 
-        public static bool logout()
-        {
-            bool ret;
-            if (ret = HttpContext.Current.Session["Teacher"] != null) HttpContext.Current.Session["Teacher"] = null;
-            return ret;
-        }
-        public static Teacher login(String mailaddress, String password)
-        {
-            Teacher retTeacher = null;
-            string cstr = ConfigurationManager.ConnectionStrings["Db"].ConnectionString;
-            using (SqlConnection connection = new SqlConnection(cstr))
-            {
-                string sql = "select * from Teacher where mailaddress = @mailaddress";
-                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
-                adapter.SelectCommand.Parameters.Add("@mailaddress", SqlDbType.VarChar);
-                adapter.SelectCommand.Parameters["@mailaddress"].Value = mailaddress;
-                DataSet ds = new DataSet();
-                int cnt = adapter.Fill(ds, "Teacher");
-                if (cnt != 0)
-                {
-                    DataTable dt = ds.Tables["Teacher"];
-                    Byte[] passwordDigest = (Byte[])dt.Rows[0]["passwordDigest"];
-                    if (dt.Rows[0]["inactiveDate"] == DBNull.Value)
-                    {
-                        byte[] input = Encoding.ASCII.GetBytes(password);
-                        SHA256 sha = new SHA256CryptoServiceProvider();
-                        byte[] hash_sha256 = sha.ComputeHash(input);
-                        if (passwordDigest.SequenceEqual(hash_sha256))
-                        {
-                            retTeacher = new Teacher();
-                            retTeacher.id = (int)dt.Rows[0]["id"];
-                            retTeacher.name = dt.Rows[0]["name"].ToString();
-                            retTeacher.sex = (int)dt.Rows[0]["sex"];
-                            retTeacher.profileImage = dt.Rows[0]["profileImage"].ToString();
-                            retTeacher.mailaddress = dt.Rows[0]["mailaddress"].ToString();
-                            retTeacher.age = (int)dt.Rows[0]["age"];
-                            retTeacher.language = dt.Rows[0]["language"].ToString();
-                            retTeacher.intoroduction = dt.Rows[0]["introduction"].ToString();
-                            retTeacher.passwordDigest = (Byte[])dt.Rows[0]["passwordDigest"];
-                            retTeacher.nationality = dt.Rows[0]["nationality"].ToString();
-                            retTeacher.is2FA = (bool)dt.Rows[0]["is2FA"];
-                            retTeacher.point = (int)dt.Rows[0]["point"];
+		public static bool logout()
+		{
+			bool ret;
+			if (ret = HttpContext.Current.Session["Teacher"] != null) HttpContext.Current.Session["Teacher"] = null;
+			return ret;
+		}
+		public static Teacher login(String mailaddress, String password)
+		{
+			Teacher retTeacher = null;
+			string cstr = ConfigurationManager.ConnectionStrings["Db"].ConnectionString;
+			using (SqlConnection connection = new SqlConnection(cstr))
+			{
+				string sql = "select * from Teacher where mailaddress = @mailaddress";
+				SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+				adapter.SelectCommand.Parameters.Add("@mailaddress", SqlDbType.VarChar);
+				adapter.SelectCommand.Parameters["@mailaddress"].Value = mailaddress;
+				DataSet ds = new DataSet();
+				int cnt = adapter.Fill(ds, "Teacher");
+				if (cnt != 0)
+				{
+					DataTable dt = ds.Tables["Teacher"];
+					Byte[] passwordDigest = (Byte[])dt.Rows[0]["passwordDigest"];
+					if (dt.Rows[0]["inactiveDate"] == DBNull.Value)
+					{
+						byte[] input = Encoding.ASCII.GetBytes(password);
+						SHA256 sha = new SHA256CryptoServiceProvider();
+						byte[] hash_sha256 = sha.ComputeHash(input);
+						if (passwordDigest.SequenceEqual(hash_sha256))
+						{
+							retTeacher = new Teacher();
+							retTeacher.id = (int)dt.Rows[0]["id"];
+							retTeacher.name = dt.Rows[0]["name"].ToString();
+							retTeacher.sex = (int)dt.Rows[0]["sex"];
+							retTeacher.profileImage = dt.Rows[0]["profileImage"].ToString();
+							retTeacher.mailaddress = dt.Rows[0]["mailaddress"].ToString();
+							retTeacher.age = (int)dt.Rows[0]["age"];
+							retTeacher.language = dt.Rows[0]["language"].ToString();
+							retTeacher.intoroduction = dt.Rows[0]["introduction"].ToString();
+							retTeacher.passwordDigest = (Byte[])dt.Rows[0]["passwordDigest"];
+							retTeacher.nationality = dt.Rows[0]["nationality"].ToString();
+							retTeacher.is2FA = (bool)dt.Rows[0]["is2FA"];
+							retTeacher.point = (int)dt.Rows[0]["point"];
 							retTeacher.address = dt.Rows[0]["address"].ToString();
-                            if (dt.Rows[0]["inactiveDate"] != DBNull.Value)
-                            {
-                                retTeacher.inactiveDate = DateTime.Parse(dt.Rows[0]["inactiveDate"].ToString());
-                            }
-                            HttpContext.Current.Session["Teacher"] = retTeacher;
-                        }
-                    }                   
-                }               
-            }
-            return retTeacher;
-        }
-        public void setPassword(String passwordRow)
-        {
-            byte[] input = Encoding.ASCII.GetBytes(passwordRow);
-            SHA256 sha = new SHA256CryptoServiceProvider();
-            passwordDigest = sha.ComputeHash(input);
-        }
-        public static Teacher currentUser()
-        {
-            return (Teacher)HttpContext.Current.Session["Teacher"];
-        }
-         public bool create()
-        {
-            bool check = false;
-            string cstr = ConfigurationManager.ConnectionStrings["Db"].ConnectionString;
-            using (var connection = new SqlConnection(cstr))
-            using (var command = connection.CreateCommand())
-            {
-                try
-                {
-                    connection.Open();
-                    command.CommandText = "Insert Into Teacher Values (@name,@sex,@profileImage,@age,@language,@intoroduction,@passwordDigest,@mailaddress,@point,@address,@is2FA,@nationality,@inactiveDate)";
-                    command.Parameters.Add(new SqlParameter("@name", name));
-                    command.Parameters.Add(new SqlParameter("@sex", sex));
-                    command.Parameters.Add(new SqlParameter("@profileImage", profileImage));
-                    command.Parameters.Add(new SqlParameter("@age", age));
-                    command.Parameters.Add(new SqlParameter("@language", language));
-                    command.Parameters.Add(new SqlParameter("@intoroduction", intoroduction));
-                    command.Parameters.Add(new SqlParameter("@passwordDigest", passwordDigest));
-                    command.Parameters.Add(new SqlParameter("@mailaddress", mailaddress));
-                    command.Parameters.Add(new SqlParameter("@point", point));
-                    command.Parameters.Add(new SqlParameter("@address", address));
-                    command.Parameters.Add(new SqlParameter("@is2FA", is2FA));
-                    command.Parameters.Add(new SqlParameter("@nationality", nationality));
-                    command.Parameters.Add(new SqlParameter("@inactiveDate", DBNull.Value));
-                    int cnt = command.ExecuteNonQuery();
-                    if (cnt != 0)
-                    {
-                        check = true;
-                    }
-                    connection.Close();
-                }catch(SqlException){
-                    //入力情報が足りないメッセージを吐く
-                }
-            }
-            return check;
-        }
+							if (dt.Rows[0]["inactiveDate"] != DBNull.Value)
+							{
+								retTeacher.inactiveDate = DateTime.Parse(dt.Rows[0]["inactiveDate"].ToString());
+							}
+							HttpContext.Current.Session["Teacher"] = retTeacher;
+						}
+					}
+				}
+			}
+			return retTeacher;
+		}
+		public void setPassword(String passwordRow)
+		{
+			byte[] input = Encoding.ASCII.GetBytes(passwordRow);
+			SHA256 sha = new SHA256CryptoServiceProvider();
+			passwordDigest = sha.ComputeHash(input);
+		}
+		public static Teacher currentUser()
+		{
+			return (Teacher)HttpContext.Current.Session["Teacher"];
+		}
+		public bool create()
+		{
+			bool check = false;
+			string cstr = ConfigurationManager.ConnectionStrings["Db"].ConnectionString;
+			using (var connection = new SqlConnection(cstr))
+			using (var command = connection.CreateCommand())
+			{
+				try
+				{
+					connection.Open();
+					command.CommandText = "Insert Into Teacher Values (@name,@sex,@profileImage,@age,@language,@intoroduction,@passwordDigest,@mailaddress,@point,@address,@is2FA,@nationality,@inactiveDate)";
+					command.Parameters.Add(new SqlParameter("@name", name));
+					command.Parameters.Add(new SqlParameter("@sex", sex));
+					command.Parameters.Add(new SqlParameter("@profileImage", profileImage));
+					command.Parameters.Add(new SqlParameter("@age", age));
+					command.Parameters.Add(new SqlParameter("@language", language));
+					command.Parameters.Add(new SqlParameter("@intoroduction", intoroduction));
+					command.Parameters.Add(new SqlParameter("@passwordDigest", passwordDigest));
+					command.Parameters.Add(new SqlParameter("@mailaddress", mailaddress));
+					command.Parameters.Add(new SqlParameter("@point", point));
+					command.Parameters.Add(new SqlParameter("@address", address));
+					command.Parameters.Add(new SqlParameter("@is2FA", is2FA));
+					command.Parameters.Add(new SqlParameter("@nationality", nationality));
+					command.Parameters.Add(new SqlParameter("@inactiveDate", DBNull.Value));
+					int cnt = command.ExecuteNonQuery();
+					if (cnt != 0)
+					{
+						check = true;
+					}
+					connection.Close();
+				}
+				catch (SqlException)
+				{
+					//入力情報が足りないメッセージを吐く
+				}
+			}
+			return check;
+		}
 		public bool delete()
 		{
 			bool check = false;
@@ -208,7 +210,7 @@ namespace telledge.Models
 				if (cnt != 0)
 				{
 					DataTable dt = ds.Tables["teacher"];
-					if(dt.Rows[0]["avgVal"] != DBNull.Value)
+					if (dt.Rows[0]["avgVal"] != DBNull.Value)
 					{
 						valuation = Convert.ToDouble(dt.Rows[0]["avgVal"]);
 					}
@@ -350,6 +352,44 @@ namespace telledge.Models
 				}
 			}
 			return retTeacher;
+		}
+		public Room[] getRooms()
+		{
+			Room[] retRooms = null;
+			string cstr = ConfigurationManager.ConnectionStrings["Db"].ConnectionString;
+			using (SqlConnection connection = new SqlConnection(cstr))
+			{
+				string sql = "select * from Room where teacherId = @teacherId";
+				SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+				adapter.SelectCommand.Parameters.Add("@teacherId", SqlDbType.VarChar);
+				adapter.SelectCommand.Parameters["@teacherId"].Value = id;
+				DataSet ds = new DataSet();
+				int cnt = adapter.Fill(ds, "Room");
+				DataTable dt = ds.Tables["Room"];
+				if (cnt != 0)
+				{
+					retRooms = new Room[cnt];
+					for (int i = 0; i < cnt; i++)
+					{
+						retRooms[i] = new Room();
+						retRooms[i].id = (int)dt.Rows[i]["id"];
+						retRooms[i].teacherId = (int)dt.Rows[i]["teacherId"];
+						retRooms[i].roomName = (String)dt.Rows[i]["roomName"];
+						retRooms[i].tag = (String)dt.Rows[i]["tag"];
+						retRooms[i].description = (String)dt.Rows[i]["description"];
+						retRooms[i].worstTime = (int)dt.Rows[i]["worstTime"];
+						retRooms[i].extensionTime = (int)dt.Rows[i]["extensionTime"];
+						retRooms[i].point = (int)dt.Rows[i]["point"];
+						retRooms[i].beginTime = DateTime.Parse(dt.Rows[i]["beginTime"].ToString());
+						if (dt.Rows[i]["endTime"] != DBNull.Value)
+						{
+							retRooms[i].endTime = DateTime.Parse(dt.Rows[i]["endTime"].ToString());
+						}
+						retRooms[i].endScheduleTime = DateTime.Parse(dt.Rows[i]["endScheduleTime"].ToString());
+					}
+				}
+			}
+			return retRooms;
 		}
 	}
 }
